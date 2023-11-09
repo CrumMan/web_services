11ccabc5572d1c54b0b637f95eed6d232449b26d
CrumMan <122419541+CrumMan@users.noreply.github.com>
2023-11-09 00:09:28 -0700

    commit

---
+++ controllers/lesson1.js
@@ -0,0 +1,11 @@
+const joshuaRoute = (req, res) => {
+    res.send('Joshua Pyle')
+}
+
+const erinRoute = (req, res) => {
+    res.send('Erin Pyle')
+}
+module.exports = {
+    joshuaRoute,
+    erinRoute
+}
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const lesson1Controller = require('./controllers/lesson1.js')
  
-app.get('/', (req, res) => {
-  res.send("Joshua Pyle");
-});
+app.get('/', lesson1Controller.joshuaRoute);
+
+app.get('/erin',  lesson1Controller.erinRoute);
 
 const port = 3000;
 
